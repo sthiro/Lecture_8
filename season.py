85ba9abd3a0b2d82ba1cb71744a8ad4aaf60c20d
Thiroshan Suthakar <thiroshansuthakar@Thiroshans-MacBook-Air.local>
2026-02-05 10:35:51 +0530

    Rewritten the season.py using OOP

--- season.py
+++ season.py
@@ -4,25 +4,38 @@ from datetime import date
 
 p = inflect.engine()
 
-def birthday_date():
-    return date.fromisoformat(input("Date of Birth: ")) 
-
-def duration():
-    try:
-        today_date = date.today()
-        duration_second = (today_date - birthday_date()).total_seconds() # Operator overload
-        total_min = round(duration_second / 60) #Round to nearest integer
-
-        return duration_second 
-
-    except ValueError: # Invalid format
-        sys.exit("Invalid date")
 
 def main():
+    print(Duration.Birthday())
+    
+class Duration():
+    def __init__(self, DOB):
+        self.DOB = DOB
+        self.Date = date.today()
 
-    duration_min = duration()
-    duration_text = p.number_to_words(duration_min)
-    print(duration_text)
+    @classmethod
+    def Birthday(cls):
+        try:
+            return cls(date.fromisoformat(input("Date of Birth: ")))
+    
+        except ValueError: # Invalid format
+            sys.exit("Invalid date")
+
+    
+    def Cal_duration_min(self):
+        
+        today_date = self.Date
+        duration_second = (today_date - self.DOB).total_seconds() # Operator overload
+        total_min = round(duration_second / 60) #Round to nearest integer
+       # print(total_min,"Working")
+        return total_min 
+
+
+    def __str__(self):
+        duration_min = self.Cal_duration_min()
+        duration_text = p.number_to_words(duration_min)
+
+        return duration_text
 
 
 if __name__ == "__main__":
